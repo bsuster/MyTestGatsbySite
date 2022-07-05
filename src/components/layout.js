1ba4b40dbebb240a9f6d001640ce71f652711aac
bsuster <benjamin.sustersic@gmail.com>
2022-07-05 19:01:45 -0400

    Completed tutorial part 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,27 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import * as style from './layout.module.css'
 
 const Layout = ({pageTitle, children}) => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }`
+      );
+
     return (
         <div className={style.container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header className={style.siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={style.navLinks}>
                     <li className={style.navLinkItem}><Link to="/" className={style.navLinkText}>Home</Link></li>
                     <li className={style.navLinkItem}><Link to="/about" className={style.navLinkText}>About</Link></li>
+                    <li className={style.navLinkItem}><Link to="/blog" className={style.navLinkText}>Blog</Link></li>
                 </ul>
             </nav>
             <main>
@@ -17,7 +29,7 @@ const Layout = ({pageTitle, children}) => {
                 {children}
             </main>
         </div>
-    )
+    );
 }
 
 export default Layout
